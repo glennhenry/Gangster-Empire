d79e2f34fe9a6907757c159657c9b32e8bd917c5
glennhenry <glennhenrybdg05@gmail.com>
2025-08-19 14:14:02 +0700

    Install dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,20 +12,22 @@ application {
 }
 
 dependencies {
+    implementation(libs.ktor.network.tls)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.websockets)
-    implementation(libs.ktor.network.tls)
-    implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.server.content.negotiation)
-    implementation(libs.mongodb.driver.core)
-    implementation(libs.mongodb.driver.sync)
-    implementation(libs.bson)
+    implementation(libs.ktor.serialization.kotlinx.protobuf)
+    implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.server.call.logging)
     implementation(libs.ktor.server.host.common)
+    implementation(libs.ktor.server.status.pages)
     implementation(libs.ktor.server.cors)
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
+    implementation(libs.mongodb.driver.kotlin.coroutine)
+    implementation(libs.mongodb.bson.kotlinx)
+    implementation(libs.library.bcrypt)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
