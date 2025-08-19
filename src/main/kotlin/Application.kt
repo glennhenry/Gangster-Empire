a59727c7c4d1c4fb5f2fa4d99d0ab1631cefa1a4
glennhenry <glennhenrybdg05@gmail.com>
2025-08-19 14:31:21 +0700

    Serve game and web assets from server

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -4,6 +4,7 @@ import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.serialization.kotlinx.protobuf.*
 import io.ktor.server.application.*
+import io.ktor.server.http.content.staticFiles
 import io.ktor.server.plugins.calllogging.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.cors.routing.*
@@ -78,5 +79,9 @@ fun Application.module() {
                 call.respond(HttpStatusCode.NotFound)
             }
         }
+
+        // web assets
+        staticFiles("/assets", File("static/assets"))
+        staticFiles("/game", File("static/game"))
     }
 }
