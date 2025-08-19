4f811ae98706dfeb61800759b37df5cba41d4cd6
glennhenry <glennhenrybdg05@gmail.com>
2025-08-19 14:14:38 +0700

    Unify module setup in Application.kt

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,16 +1,82 @@
 package dev.gangster
 
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.serialization.kotlinx.protobuf.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.protobuf.ProtoBuf
+import java.io.File
+import kotlin.time.Duration.Companion.seconds
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
+const val SERVER_HOST = "127.0.0.1"
+const val FILE_SERVER_HOST = "127.0.0.1:8080"
+const val API_SERVER_HOST = "127.0.0.1:8080"
+const val SOCKET_SERVER_HOST = "127.0.0.1:7777"
+
+const val FILE_SERVER_PORT = 8080
+const val API_SERVER_PORT = 8080
+const val SOCKET_SERVER_PORT = 7777
+
 fun Application.module() {
-    configureSockets()
-    configureSerialization()
-    configureDatabases()
-    configureMonitoring()
-    configureHTTP()
-    configureRouting()
+    // 1. Configure Websockets
+    install(WebSockets) {
+        pingPeriod = 15.seconds
+        timeout = 15.seconds
+        masking = true
+    }
+
+    // 2. Configure Serialization
+    val json = Json {
+        prettyPrint = true
+        isLenient = true
+        ignoreUnknownKeys = true
+        encodeDefaults = true
+    }
+
+    @OptIn(ExperimentalSerializationApi::class)
+    install(ContentNegotiation) {
+        json(json)
+        protobuf(ProtoBuf)
+    }
+
+    // 3. Configure HTTP
+    install(CORS) {
+        allowHost(API_SERVER_HOST, schemes = listOf("http"))
+        allowHost(SOCKET_SERVER_HOST, schemes = listOf("http"))
+        allowHeader(HttpHeaders.ContentType)
+        allowMethod(HttpMethod.Get)
+    }
+    install(StatusPages) {
+        exception<Throwable> { call, cause ->
+            call.respondText(text = "500: $cause", status = HttpStatusCode.InternalServerError)
+        }
+    }
+
+    // 4. Configure Logging
+    install(CallLogging)
+
+    // 5. Configure API routes
+    routing {
+        get("/") {
+            val indexFile = File("static/index.html")
+            if (indexFile.exists()) {
+                call.respondFile(indexFile)
+            } else {
+                call.respond(HttpStatusCode.NotFound)
+            }
+        }
+    }
 }
