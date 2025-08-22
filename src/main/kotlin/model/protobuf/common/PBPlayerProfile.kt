148d60f69ef3f7fc270876e81b4db9975c138c0e
glennhenry <glennhenrybdg05@gmail.com>
2025-08-22 16:45:48 +0700

    Fix wrong protonumber definition in PBPlayerProfile

--- src/main/kotlin/model/protobuf/common/PBPlayerProfile.kt
+++ src/main/kotlin/model/protobuf/common/PBPlayerProfile.kt
@@ -2,29 +2,32 @@ package dev.gangster.model.protobuf.common
 
 import dev.gangster.model.components.PortraitData
 import dev.gangster.utils.AdminData
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.protobuf.ProtoNumber
 
 /**
  * playerprofile command
  * [example: %xt%playerprofile%1%-1%CAEQsOSlAiKCAQoKSmVubnlIYXplXxACGikyITB+Mn4wfjJ+MX4yfjB+MCEwfjN+Mn4wfjR+NH4wfjJ+M34wfjJ+NiACKgoIlQIQ5gIYQSBaMhYIwg0QzMwBHRsbP0ElA7A0QS0AAMhCSg8KBQgeEPZEEgYIpDcQ3XBYtwFgTmgAcBJ4AYgB/////wc=%]
  */
+@OptIn(ExperimentalSerializationApi::class)
 @Serializable
 data class PBPlayerProfile(
-    val name: String,
-    val gender: PBGender,
-    val picString: String,
-    val characterClass: PBCharacterClass,
-    val attributes: PBAttributes,
-    val combatStats: PBCombatStats,
-    val highscore: PBHighscore,
-    val wonDuels: Int,
-    val lostDuels: Int,
-    val isIgnored: Boolean,
-    val level: Int,
-    val cityId: PBCity,
-    val clanName: String?,
-    val clanId: Int?,
-    val rankInClan: PBFamilyRank?,
+    @ProtoNumber(1) val name: String,
+    @ProtoNumber(2) val gender: PBGender,
+    @ProtoNumber(3) val picString: String,
+    @ProtoNumber(4) val characterClass: PBCharacterClass,
+    @ProtoNumber(5) val attributes: PBAttributes,
+    @ProtoNumber(6) val combatStats: PBCombatStats,
+    @ProtoNumber(9) val highscore: PBHighscore, // 7, 8 is skipped in client
+    @ProtoNumber(11) val wonDuels: Int,
+    @ProtoNumber(12) val lostDuels: Int,
+    @ProtoNumber(13) val isIgnored: Boolean,
+    @ProtoNumber(14) val level: Int,
+    @ProtoNumber(15) val cityId: PBCity,
+    @ProtoNumber(16) val clanName: String?,
+    @ProtoNumber(17) val clanId: Int?,
+    @ProtoNumber(18) val rankInClan: PBFamilyRank?,
 ) {
     companion object {
         fun dummy(): PBPlayerProfile {
