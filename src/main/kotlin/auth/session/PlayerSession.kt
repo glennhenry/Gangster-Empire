fb02fa69819b7bad7067d52638aa80963b762c4b
glennhenry <glennhenrybdg05@gmail.com>
2025-08-24 17:49:16 +0700

    Remove session as it is not needed by the game

--- src/main/kotlin/auth/session/PlayerSession.kt
+++
@@ -1,9 +0,0 @@
-package dev.gangster.auth.session
-
-data class PlayerSession(
-    val playerId: String,
-    val token: String,
-    val issuedAt: Long,
-    var expiresAt: Long,
-    var lifetime: Long,
-)
--- src/main/kotlin/auth/session/SessionManager.kt
+++
@@ -1,5 +0,0 @@
-package dev.gangster.auth.session
-
-class SessionManager {
-
-}
