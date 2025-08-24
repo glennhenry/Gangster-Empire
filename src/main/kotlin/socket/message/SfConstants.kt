baaf535bc8cc2dba3e68944024702493d807deec
glennhenry <glennhenrybdg05@gmail.com>
2025-08-24 22:59:51 +0700

    Create SfConstants and XtConstants

---
+++ src/main/kotlin/socket/message/SfConstants.kt
@@ -0,0 +1,9 @@
+package dev.gangster.socket.message
+
+object SfConstants {
+    const val TYPE_SYS = "sys"
+    const val ACTION_VER_CHK = "verChk"
+    const val ACTION_LOGIN = "login"
+    const val ACTION_AUTO_JOIN = "autoJoin"
+    const val ACTION_ROUND_TRIP = "autoJoin"
+}
---
+++ src/main/kotlin/socket/message/XtConstants.kt
@@ -0,0 +1,47 @@
+package dev.gangster.socket.message
+
+object XtConstants {
+    const val ZONE = "MafiaEx" // follows (static/gangster-data/games-netconf/4/1.xml)
+
+    const val COMMAND_RLU = "rlu"
+    const val COMMAND_PIN = "pin"
+    const val COMMAND_VCK = "vck"
+
+    const val COMMAND_CREATE_AVATAR = "createavatar"
+    const val COMMAND_LOGIN_REGISTER = "lre"
+    const val COMMAND_LOGIN = "lgn"
+
+    const val COMMAND_AVATAR_ACHIEVEMENTS = "oga"
+    const val COMMAND_GOLD_CONSTANTS_DATA = "sgc"
+    const val COMMAND_PLAYER_INFO = "oio"
+    const val COMMAND_PLAYER_PROFILE = "playerprofile"
+    const val COMMAND_NEW_ACHIEVEMENTS = "newachievements"
+
+    const val COMMAND_PAYMENT_INFO = "paymentinfo"
+    const val COMMAND_MAFIA_USER_DATA = "oud"
+    const val COMMAND_PLAYER_CURRENCY = "playercurrency"
+    const val COMMAND_VIEW_ARMAMENT = "viewarmament"
+    const val COMMAND_GET_ARMAMENT_PRESET_STATUS = "getarmamentpresetstatus"
+
+    const val COMMAND_VIEW_GEAR = "viewgear"
+    const val COMMAND_VIEW_FOOD = "viewfood"
+    const val COMMAND_VIEW_INVENTORY = "viewinventory"
+    const val COMMAND_VIEW_ITEMS = "viewitems"
+    const val COMMAND_ATTRIBUTE_COSTS_DATA = "auc"
+
+    const val COMMAND_GET_PLAYER_BOOSTER = "getplayerbooster"
+    const val COMMAND_SHOW_MISSION_BOOSTER = "showmissionbooster"
+    const val COMMAND_VIEW_MISSIONS = "viewmissions"
+    const val COMMAND_VIEW_WORK = "viewwork"
+    const val COMMAND_PING = "png"
+
+    const val COMMAND_SPECIAL_EVENT_DATA = "sae"
+    const val COMMAND_LOGIN_FEATURES_DATA = "lfe"
+    const val COMMAND_PAYMENT_HASH = "gch"
+    const val COMMAND_CRYPTED_FORUM_HASH = "gfl"
+    const val COMMAND_GET_ACTIVE_QUESTS = "getactivequests"
+
+    const val COMMAND_GANG_SHOP = "sgs"
+    const val COMMAND_PLAYER_GANG = "sga"
+    const val COMMAND_ALL_PLAYER_DATA = "apd"
+}
