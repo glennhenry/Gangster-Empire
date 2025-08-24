1599f9bfefa85dc55a7994dd9f8e69a1c6850827
glennhenry <glennhenrybdg05@gmail.com>
2025-08-24 17:57:50 +0700

    Update usage of AdminData

--- src/main/kotlin/data/collection/PlayerData.kt
+++ src/main/kotlin/data/collection/PlayerData.kt
@@ -1,11 +1,15 @@
 package dev.gangster.data.collection
 
+import dev.gangster.game.data.AdminData
+
 data class PlayerData(
+    val playerId: Long,
     val x: Int = 0
 ) {
     companion object {
         fun admin(): PlayerData {
             return PlayerData(
+                playerId = AdminData.PLAYER_ID_NUMBER,
                 x = 0
             )
         }
